720bfaf49e5afddf2e517d406037fdc4ade48b37
rlss91 <rlss91@gmail.com>
2022-01-20 15:10:30 +0200

    crud

--- routes/api/user.js
+++ routes/api/user.js
@@ -15,4 +15,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+/* DELETE users */
+router.delete("/", async (req, res) => {
+  try {
+    const userData = await UserModel.selectUserByEmail(req.jwtData.email);
+    if (userData.length > 0) {
+      UserModel.deleteUserById(userData[0]._id);
+      res.json({ msg: "user deleted" });
+    } else {
+      res.json({ msg: "user was not there" });
+    }
+  } catch (err) {
+    res.json({ status: "error", err });
+  }
+});
+
 module.exports = router;
